f43922c3b6014aa72251de691d19d21d3e576ab9
pixel <pixelisprocrastinating15@gmail.com>
2025-07-31 20:49:08 +0200

    Update LevelAuthors.js

--- js/components/List/LevelAuthors.js
+++ js/components/List/LevelAuthors.js
@@ -10,37 +10,23 @@ export default {
         },
     },
     template: `
- <div class="level-authors">
-            <template v-if="selfVerified">
-                <div class="type-title-sm">Creator & Verifier</div>
-                <p class="type-body">
-                    <span>{{ author }}</span>
-                </p>
-            </template>
+  <div class="level-authors">
             <template v-else-if="creators.length === 0">
-                <div class="type-title-sm">Composers</div>
+                <div class="type-title-sm">Composer</div>
                 <p class="type-body">
                     <span>{{ author }}</span>
                 </p>
-            </template>
             <template v-else>
-                <div class="type-title-sm">Creators</div>
-                <p class="type-body">
-                    <template v-for="(creator, index) in creators" :key="\`creator-\$\{creator\}\`">
-                        <span >{{ creator }}</span
-                        ><span v-if="index < creators.length - 1">, </span>
-                    </template>
-                </p>
-            </template>
-            <div class="type-title-sm">Charters</div>
+				<div class="type-title-sm">Charter</div>
             <p class="type-body">
                 <span>{{ author }}</span>
             </p>
         </div>
     `,
+
     computed: {
         selfVerified() {
             return this.author === this.verifier && this.creators.length === 0;
         },
     },
-}
+};
