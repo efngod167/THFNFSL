4acb2f193f7407f053a92d830b5c0dedcc9a5676
pixel <pixelisprocrastinating15@gmail.com>
2025-07-31 19:20:21 +0200

    Update LevelAuthors.js

--- js/components/List/LevelAuthors.js
+++ js/components/List/LevelAuthors.js
@@ -10,27 +10,33 @@ export default {
         },
     },
     template: `
- <div class="level-authors">
-            </template>
+  <div class="level-authors">
             <template v-else-if="creators.length === 0">
                 <div class="type-title-sm">Composer</div>
                 <p class="type-body">
                     <span>{{ author }}</span>
                 </p>
-            </template>
             <template v-else>
                 <div class="type-title-sm">Composers</div>
                 <p class="type-body">
-                    <template v-for="(creator, index) in creators" :key="\`creator-\$\{creator\}\`">
-                        <span >{{ creator }}</span
-                        ><span v-if="index < creators.length - 1">, </span>
+                    <template v-for="(composer, index) in composers" :key="\`composer-\$\{composer\}\`">
+                        <span >{{ author }}</span
+                        ><span v-if="index < composers.length - 1">, </span>
                     </template>
                 </p>
             </template>
-            <div class="type-title-sm">Charters</div>
+            <div class="type-title-sm">Charter</div>
             <p class="type-body">
                 <span>{{ author }}</span>
             </p>
+			<template v-else>
+                <div class="type-title-sm">Charters</div>
+                <p class="type-body">
+                    <template v-for="(charter, index) in charters" :key="\`charter-\$\{charter\}\`">
+                        <span >{{ author }}</span
+                        ><span v-if="index < charters.length - 1">, </span>
+                    </template>
+                </p>
         </div>
     `,
 
