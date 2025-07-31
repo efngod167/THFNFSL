6cab81d4c90050b2937c9c0446c8cbbf84c4c3ef
pixel <pixelisprocrastinating15@gmail.com>
2025-07-31 17:59:02 +0200

    Update LevelAuthors.js

--- js/components/List/LevelAuthors.js
+++ js/components/List/LevelAuthors.js
@@ -18,31 +18,16 @@ export default {
                 </p>
             </template>
             <template v-else-if="creators.length === 0">
-                <div class="type-title-sm">Composer</div>
+                <div class="type-title-sm">Composers</div>
                 <p class="type-body">
                     <span>{{ author }}</span>
                 </p>
             </template>
-            <template v-else>
-                <div class="type-title-sm">Composers</div>
-                <p class="type-body">
-                    <template v-for="(creator, index) in creators" :key="\`creator-\$\{creator\}\`">
-                        <span >{{ creator }}</span
-                        ><span v-if="index < creators.length - 1">, </span>
-                    </template>
-                </p>
-            </template>
-            <div class="type-title-sm">Charter</div>
+            <div class="type-title-sm">Charters</div>
             <p class="type-body">
                 <span>{{ charter }}</span>
             </p>
         </div>
-        <div class="type-title-sm">Charters</div>
-                <p class="type-body">
-                    <template v-for="(charter, index) in charters" :key="\`charter-\$\{charter\}\`">
-                        <span >{{ charter }}</span
-                        ><span v-if="index < charters.length - 1">, </span>
-                    </template>
     `,
 
     computed: {
