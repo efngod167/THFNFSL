741b635e2bbe9cbeaf3e46904bd433ccfc6fa32e
pixel <pixelisprocrastinating15@gmail.com>
2025-08-01 12:53:26 +0200

    Update LevelAuthors.js

--- js/components/List/LevelAuthors.js
+++ js/components/List/LevelAuthors.js
@@ -1,23 +1,38 @@
 export default {
     props: {
-        charters: {
+        author: {
             type: String,
             required: true,
         },
-        composers: {
+        creators: {
             type: Array,
             required: true,
         },
     },
     template: `
-  <div class="level-authors">
-            <template v-else-if="creators.length === 0">
-                <div class="type-title-sm">Composer</div>
+        <div class="level-authors">
+            <template v-if="selfVerified">
+                <div class="type-title-sm">Creator & Verifier</div>
                 <p class="type-body">
                     <span>{{ author }}</span>
                 </p>
+            </template>
+            <template v-else-if="creators.length === 0">
+                <div class="type-title-sm">Composers</div>
+                <p class="type-body">
+                    <span>{{ author }}</span>
+                </p>
+            </template>
             <template v-else>
-				<div class="type-title-sm">Charter</div>
+                <div class="type-title-sm">Creators</div>
+                <p class="type-body">
+                    <template v-for="(creator, index) in creators" :key="\`creator-\$\{creator\}\`">
+                        <span >{{ creator }}</span
+                        ><span v-if="index < creators.length - 1">, </span>
+                    </template>
+                </p>
+            </template>
+            <div class="type-title-sm">Charters</div>
             <p class="type-body">
                 <span>{{ author }}</span>
             </p>
